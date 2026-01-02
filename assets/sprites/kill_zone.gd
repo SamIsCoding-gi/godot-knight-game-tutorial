214f7e40e93169e4cc92ae2d40aac1fee55bd816
Samuel Kibunda <111416591+SamIsCoding-gi@users.noreply.github.com>
2026-01-02 20:13:36 +0200

    Implement player controls and animations; add game manager script

--- assets/sprites/kill_zone.gd
+++ assets/sprites/kill_zone.gd
@@ -6,6 +6,7 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	print("you died")
 	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
--- scripts/knight_player.gd
+++ scripts/knight_player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -260.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -11,12 +12,28 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+
+	# Get player input directyion which will be either -1, 0, 1
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	# Flip sprite animation based on direction -1, 0, 1
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	if is_on_floor():
+		#Play jump animation
+		if direction == 0:
+			animated_sprite.play("knight_idle")
+		else:
+			animated_sprite.play("knight_run")
+	else: 
+		animated_sprite.play("knight_jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
